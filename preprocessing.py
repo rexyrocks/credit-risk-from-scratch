d63765b72ed564286bfebd27e9487eebeef9deb6
rexycodes <piyochoudhary@gmail.com>
2026-09-02 10:25:14 +0530

    preprocessing complete: scaling + train-test split

--- preprocessing.py
+++ preprocessing.py
@@ -8,8 +8,30 @@ def cleandata(df):
     df["MonthlyIncome"] = df["MonthlyIncome"].fillna(df["MonthlyIncome"].median())
     df["NumberOfDependents"] = df["NumberOfDependents"].fillna(df["NumberOfDependents"].mode()[0])
     return df
+def scalefeature(X):
+    mean = X.mean(axis = 0)
+    std = X.std(axis =0)
+    X_sclaed = (X-mean)/std
+    return X_sclaed,mean,std
+def train_test_split(X,y,test_size = 0.2 , seed = 42):
+    np.random.seed(seed)
+    n = X.shape[0]
+    indices = np.random.permutation(n)
+    test_count = int(n*test_size)
+    test_idx = indices[:test_count]
+    train_idx = indices[test_count:]
+    return X[train_idx] , X[test_idx], y[train_idx], y[test_idx]
 if __name__ == "__main__":
     df = load_data("data/cs-training.csv")
     print(df.shape)
     df = cleandata(df)
     print(df.isnull().sum())
+    
+    y = df["SeriousDlqin2yrs"].values          # ✅ yahan bhi indent
+    X = df.drop(columns=["SeriousDlqin2yrs"]).values
+    
+    X_scaled, mean, std = scalefeature(X)
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y)
+    
+    print("X_train:", X_train.shape)            # ✅ yeh bhi add kar, warna kuch print hi nahi hoga
+    print("X_test:", X_test.shape)
